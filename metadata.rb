c8d6b98f84f66ffe65d1934b8468a52389380276
David Joos <david.joos@gmail.com>
2013-03-13 19:23:42 +0000

    metadata attribute change(s)

--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,19 @@
+name             "phpmd"
 maintainer       "Escape Studios"
 maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures phpmd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
-supports "ubuntu"
-supports "debian"
-supports "centos"
-supports "redhat"
-supports "fedora"
-supports "scientific"
-supports "amazon"
+%w{ debian ubuntu redhat centos fedora scientific amazon }.each do |os|
+supports os
+end
 
 depends "php"
 depends "composer"
 depends "pdepend"
+
+recipe "phpmd", "Installs phpmd."
+recipe "phpmd::composer", "Installs phpmd using composer."
+recipe "phpmd::pear", "Installs phpmd using pear."
