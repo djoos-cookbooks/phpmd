2de3822d65d63df0096b8ba04aa3096de2979333
David Joos <david.joos@gmail.com>
2016-09-20 11:25:25 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'development@davidjoos.com'
 license 'MIT'
 description 'Installs/Configures phpmd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.6'
+version '0.1.0'
 
 %w(debian ubuntu redhat centos fedora scientific amazon).each do |os|
   supports os
