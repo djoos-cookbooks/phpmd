3a1abd46e917acee226f74d9bdc74f8bec3da3e7
Dieter Blomme <dieterblomme@gmail.com>
2016-10-04 18:06:03 +0200

    Use composer_install_global

--- attributes/composer.rb
+++ attributes/composer.rb
@@ -5,4 +5,7 @@
 # Copyright (c) 2016, David Joos
 #
 
-default['phpmd']['install_dir'] = '/usr/local/phpmd'
+default['phpmd']['install_dir'] = default['composer']['global_install']['install_dir']
+if default['phpmd']['install_method'] == 'composer'
+  default['phpmd']['bin_dir'] = default['composer']['global_install']['bin_dir']
+end
